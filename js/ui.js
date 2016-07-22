e57c6f472fe11f25320e69913e8ae59fb2bec391
maystrovakate <maystrovakate@gmail.com>
2016-07-22 13:13:59 +0300

    start

---
+++ js/ui.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $(".nav1").click(function() {
+        $(".slideshow ul li").removeClass("active");
+        $(".image1").addClass("active");
+        $(".nav i").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".nav2").click(function() {
+        $(".slideshow ul li").removeClass("active");
+        $(".image2").addClass("active");
+        $(".nav i").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".nav3").click(function() {
+        $(".slideshow ul li").removeClass("active");
+        $(".image3").addClass("active");
+        $(".nav i").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".nav4").click(function() {
+        $(".slideshow ul li").removeClass("active");
+        $(".image4").addClass("active");
+        $(".nav i").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".nav5").click(function() {
+        $(".slideshow ul li").removeClass("active");
+        $(".image5").addClass("active");
+        $(".nav i").removeClass("active");
+        $(this).addClass("active");
+    });
+    $(".nav6").click(function() {
+        $(".slideshow ul li").removeClass("active");
+        $(".image6").addClass("active");
+        $(".nav i").removeClass("active");
+        $(this).addClass("active");
+    });
+});
